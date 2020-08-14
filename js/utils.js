53af6d7f03b0ec5a6728fa06d00f84a6d2c012d5
Awangyue-cn <Awangyue_cn@163.com>
2020-08-14 17:19:15 +0800

    Site updated: 2020-08-14 17:19:15

--- js/utils.js
+++ js/utils.js
@@ -1,62 +1,64 @@
-/**
- * created by lvfan
- * 2018-09-04
- */
+(function ($) {
+  "use strict";
 
-/**
- * @description 获取实时时间，写入 id 为 now-time 的标签中
- */
-(function () {
-    const divTime = document.getElementById('now-time');
-
-    function getTime() {
-        let time = new Date();
-        let hour = time.getHours() < 10 ? '0' + time.getHours() : time.getHours();
-        let minute = time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes();
-        let second = time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds();
-        divTime.innerText = hour + ':' + minute + ':' + second;
-    }
-
-    getTime();
-    setInterval(function () {
-        getTime();
-    }, 1000);
-}());
-
-/**
- * @description 判断当前页面是否为活动页
- */
-(function () {
-    // 网页当前状态判断
-    let state, visibilityChange, status, doc = document;
-    if (typeof document.hidden !== 'undefined') {
-        visibilityChange = 'visibilitychange';
-        state = 'visibilityState';
-    } else if (typeof document.mozHidden !== 'undefined') {
-        visibilityChange = 'mozvisibilitychange';
-        state = 'mozVisibilityState';
-    } else if (typeof document.msHidden !== 'undefined') {
-        visibilityChange = 'msvisibilitychange';
-        state = 'msVisibilityState';
-    } else if (typeof document.webkitHidden !== 'undefined') {
-        visibilityChange = 'webkitvisibilitychange';
-        state = 'webkitVisibilityState';
-    }
-    let docText = doc.title;
-    // 添加监听器，在title里显示状态变化
-    doc.addEventListener(visibilityChange, function () {
-        if (doc[state] === 'visible') {
-            doc.title = '欢迎回来！d(`･∀･)b 👏';
-            status = setTimeout(() => {
-                doc.title = docText;
-            }, 1000);
+  ZHAOO.utils = {
+    debounce: function (func, wait, immediate) {
+      var timeout;
+      return function () {
+        var context = this;
+        var args = arguments;
+        if (timeout) clearTimeout(timeout);
+        if (immediate) {
+          var callNow = !timeout;
+          timeout = setTimeout(function () {
+            timeout = null;
+          }, wait);
+          if (callNow) func.apply(context, args);
         } else {
-            doc.title = '藏起来了d(`x_x)b';
-            if (status) {
-                clearTimeout(status);
-            }
+          timeout = setTimeout(function () {
+            func.apply(context, args);
+          }, wait);
         }
-    }, false);
-    // 初始化页面状态
-    // doc.title = '吕钒的后花园';
-}());
+      }
+    },
+    throttle: function (func, wait, options) {
+      var timeout, context, args;
+      var previous = 0;
+      if (!options) options = {};
+      var later = function () {
+        previous = options.leading === false ? 0 : new Date().getTime();
+        timeout = null;
+        func.apply(context, args);
+        if (!timeout) context = args = null;
+      }
+      var throttled = function () {
+        var now = new Date().getTime();
+        if (!previous && options.leading === false) previous = now;
+        var remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+        if (remaining <= 0 || remaining > wait) {
+          if (timeout) {
+            clearTimeout(timeout);
+            timeout = null;
+          }
+          previous = now;
+          func.apply(context, args);
+          if (!timeout) context = args = null;
+        } else if (!timeout && options.trailing !== false) {
+          timeout = setTimeout(later, remaining);
+        }
+      }
+      return throttled;
+    },
+    loadScript: function (src, callback) {
+      var script = document.createElement('script');
+      document.head.appendChild(script);
+      script.src = src;
+      script.onload = function () {
+        callback();
+      }
+    }
+  }
+
+})(jQuery);
